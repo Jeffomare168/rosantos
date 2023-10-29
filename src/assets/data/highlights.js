69bcda78ca182310df3cc11bdd05421c41733c1f
Omare Geoffrey Oruko <63578815+Jeffomare168@users.noreply.github.com>
2023-10-29 07:41:13 -0700

    Update highlights.js

--- src/assets/data/highlights.js
+++ src/assets/data/highlights.js
@@ -3,28 +3,27 @@ const highlights = [
         title: "Grilled Chicken Breast",
         description: "Grilled chicken breast is a delicious and wholesome dish made from boneless, skinless chicken breast that has been marinated and cooked on a grill. The chicken breast is typically seasoned with a blend of herbs, spices, and often a marinade like lemon juice, olive oil, garlic, and various seasonings to enhance its flavor.",
         price: 350,
-        img: "https://res.cloudinary.com/dyo0ezwgs/image/upload/v1694251875/rosantos/GRILLED_CHICKEN_SALAD_zgcqdb.jpg"
+        img: "https://res.cloudinary.com/dnuvnajtt/image/upload/v1694364562/rosantos/GRILLED_CHICKEN_SALAD_zgcqdb_j8ufbd.jpg"
     },
     {
         title: "Maryland Chicken",
         description: "Maryland chicken is a classic Southern American dish known for its crispy fried chicken served with a rich and creamy white gravy. The chicken is typically seasoned with a blend of spices and herbs before being fried to golden perfection, resulting in a mouthwatering combination of crunchy texture and savory flavors.",
         price: 500,
-        img: "https://res.cloudinary.com/dyo0ezwgs/image/upload/v1694251888/rosantos/MARYLAND_CHICKEN_j8zhy9.jpg"
+        img: "https://res.cloudinary.com/dnuvnajtt/image/upload/v1694364564/rosantos/MARYLAND_CHICKEN_j8zhy9_lfdfvn.jpg"
         
     },
     {
         title: "Mbuzi Arosto",
         description: "Goat meat dishes, enjoyed in various cuisines around the world, offer a unique and robust flavor profile. Often prepared through slow cooking or grilling, goat meat is tenderized and infused with a rich blend of spices and seasonings, making it a savory and satisfying culinary experience for those who appreciate its distinct taste.",
         price: 400,
-        img: "https://res.cloudinary.com/dyo0ezwgs/image/upload/v1694251865/rosantos/MBUZI_AROSTO_fkvva3.jpg"
+        img: "https://res.cloudinary.com/dnuvnajtt/image/upload/v1694364566/rosantos/MBUZI_AROSTO_fkvva3_cotoil.jpg"
     },
     {
         title: "Rosantos Chicken Wings",
         description: "Chicken wings are a popular finger food loved for their crispy, tender texture and flavorful meat. They come in various styles, from classic buffalo wings coated in spicy sauce to sweet and tangy barbecue wings, making them a versatile and crowd-pleasing appetizer or snack.",
         price: 500,
-        img: "https://res.cloudinary.com/dyo0ezwgs/image/upload/v1694251888/rosantos/ROASTED_CHICKEN_WINGS_fuv7px.jpg"
+        img: "https://res.cloudinary.com/dnuvnajtt/image/upload/v1694364574/rosantos/ROASTED_CHICKEN_WINGS_fuv7px_hhoi4a.jpg"
 
     }
 ]
-
-export default highlights; 
+export default highlights;
